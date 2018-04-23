fe596c3acd639ed56cac01c7cf9b8e095e411dd4
sinsens <xzapk@hotmail.com>
2018-04-23 16:38:04 +0800

    首次提交

---
+++ MyClass.py
@@ -0,0 +1,86 @@
+# -*- encoding: utf-8 -*-
+from os import system
+import threading
+from flask import render_template
+from os import path
+import os
+from flask import request
+import sys
+import time
+
+
+def now():
+	return time.time()
+
+class Server():
+	def __init__(self, dirpath):
+		self.homeroot = dirpath+'/static/'
+		self.chk(dirpath)
+	
+	''' 目录检查 '''
+	def chk(self, dirpath):
+		# 检查读写
+		if path.exists(self.homeroot) and path.isdir(self.homeroot):
+			pass
+		else:
+			try:
+				os.mkdir(self.homeroot)
+			except:
+				return "File IOError"
+		print("\n\n\n")
+		print("Working directory:", dirpath)
+		print("File Downloaded and saved directory:", self.homeroot)
+		print("\n")
+
+	def index(self):
+		mylist = self.list()
+		return render_template('index.html', mylist = mylist)
+
+	def go(self):
+		url = request.form['url']
+		threading.Thread(target = self.down, args=([url])).start()
+		ip = request.remote_addr
+		print(ip,": Get file from ",url)
+		return self.index()
+
+	def fileinfo(self, filepath):
+		filepath = self.homeroot + filepath
+		if path.exists(filepath) and path.isfile(filepath):
+			return path.basename(filepath), path.getsize(filepath), os.stat(filepath).st_mtime
+		else:
+			return None
+
+	def list(self):
+		flist = []
+		for i in os.listdir(self.homeroot):
+			finfo = self.fileinfo(i)
+			size = finfo[1]
+			date = time.localtime(finfo[2])
+			date = time.strftime("%Y-%m-%d %H:%M:%S",date)
+			if size>(1024*1024*1024):
+				size = str(round(size/1024/1024/1024,2))+"GB"
+			elif (size)>(1024*1024):
+				size = str(round(size/1024/1024,2))+"MB"
+			else:
+				size = str(round(size/1024,2))+"KB"
+			flist.append({'filename':finfo[0], 'size':size, 'date':date})
+		return (flist)
+
+	def down(self, url):
+		# 判断URL
+		if(len(url)<15):
+			return
+		# 安全过滤
+		if ";" in url:
+			return
+		
+		try:
+			if("youtube.com" in url):
+				print("Downloding video from Youtube, this thread will switch word dir to ", self.homeroot)
+				os.chdir(self.homeroot)
+				system('youtube-dl %s' % (url))
+			else:
+				system('you-get -o %s %s' % (self.homeroot, url))
+		except Exception:
+			print("Some problam happend -_-||")
+			raise
---
+++ app.py
@@ -0,0 +1,30 @@
+# -*- encoding: utf-8 -*-
+
+from flask import Flask
+from MyClass import Server
+import sys
+
+
+app = Flask(__name__)
+port = 8081
+app.debug = True
+s = Server(sys.path[0]) #传递当前目录
+
+@app.route('/')
+def hello():
+	return s.index()
+
+@app.route('/get/<filename>', methods=['GET', 'POST'])
+def send(filename):
+	return app.send_static_file(filename)
+
+@app.route('/list')
+def list():
+	return s.list()
+
+@app.route('/go', methods = ['POST'])
+def go():
+	return s.go()
+
+if __name__ == '__main__':
+	app.run(port=port)
