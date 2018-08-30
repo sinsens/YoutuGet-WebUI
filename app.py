8a8dd124384ebb7af337295afb7cb26b88016649
sinsens <xzapk@hotmail.com>
2018-08-30 20:21:59 +0800

    转为utf-8 BOM编码

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+﻿from flask import Flask
 from MyClass import Server
 import sys
 
@@ -6,7 +6,7 @@ import sys
 app = Flask(__name__)
 port = 8081
 app.debug = True
-s = Server(sys.path[0]) #���ݵ�ǰĿ¼
+s = Server(sys.path[0]) #传递当前目录
 
 @app.route('/')
 def hello():
